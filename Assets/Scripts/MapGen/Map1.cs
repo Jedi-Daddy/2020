ad1abd0d8c74050c958bb30440e1d2d0b6632b4d
Stanisalv Herasymenko <s.gerasimenko@plarium.com>
2020-10-24 17:06:57 +0300

    Minor improvement to player spawn logic to make it work with any grid size. If you want you can also make it work with any cell size

--- Assets/Scripts/MapGen/Map1.cs
+++ Assets/Scripts/MapGen/Map1.cs
@@ -1,11 +1,10 @@
-﻿using Assets.Scripts.Colliders;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class Map1 : MonoBehaviour
 {
   public string level =
-@"1	1	5	1	6	0	0	0	0
+ @"1	1	5	1	6	0	0	0	0
  0	0	0	1	0	0	0	1	0
  0	1	0	0	0	1	0	1	0
  0	0	1	0	1	0	0	0	1
@@ -24,44 +23,43 @@ public class Map1 : MonoBehaviour
 
   public void Start()
   {
-    Vector3 playerVector = new Vector3(0,0,0);
+    Vector3 playerVector = new Vector3(0, 0, 0);
     var map = ReadFromFile(level);
     for (int y = 0; y < map.Length; y++)
     {
-        for (int x = 0; x < map[y].Length; x++)
+      for (int x = 0; x < map[y].Length; x++)
+      {
+        switch (map[y][x])
         {
-          switch (map[y][x])
-          {
-            case 0:
-              var block = Instantiate(floor_valid, new Vector3(x, map.Length - y, 0), Quaternion.identity);
-              block.transform.SetParent(Parent.transform);
-              break;
-            case 1:
-              var obstacle = Instantiate(floor_obstacle, new Vector3(x, map.Length - y, 0), Quaternion.identity);
-              obstacle.transform.SetParent(Parent.transform);
-              break;
-            case 5:
-              var characterBackground = Instantiate(floor_valid, new Vector3(x, map.Length-y, 0), Quaternion.identity);
-              characterBackground.transform.SetParent(Parent.transform);
-              
-              var characterBlock = Instantiate(character, Vector3.zero, Quaternion.identity);
-              characterBlock.transform.SetParent(Parent.transform.parent);
-              var parentRectTransform = Parent.GetComponent<RectTransform>();
-              var rectTransform = characterBlock.GetComponent<RectTransform>();
-              if (rectTransform != null && parentRectTransform != null)
-              {
-                rectTransform.anchoredPosition = new Vector2(-400f, 450f) + new Vector2 (x * 100, -y * 100);
-              }
-
-              playerVector = new Vector3(x, map.Length - y, 0);
+          case 0:
+            var block = Instantiate(floor_valid, new Vector3(x, map.Length - y, 0), Quaternion.identity);
+            block.transform.SetParent(Parent.transform);
             break;
-            case 6:
-              var exit = Instantiate(floor_exit, new Vector3(x, map.Length - y, 0), Quaternion.identity);
-              exit.transform.SetParent(Parent.transform);
-              break;
-          }
+          case 1:
+            var obstacle = Instantiate(floor_obstacle, new Vector3(x, map.Length - y, 0), Quaternion.identity);
+            obstacle.transform.SetParent(Parent.transform);
+            break;
+          case 5:
+            var characterBackground = Instantiate(floor_valid, new Vector3(x, map.Length - y, 0), Quaternion.identity);
+            characterBackground.transform.SetParent(Parent.transform);
+
+            var characterBlock = Instantiate(character, Vector3.zero, Quaternion.identity);
+            characterBlock.transform.SetParent(Parent.transform.parent);
+            var rectTransform = characterBlock.GetComponent<RectTransform>();
+            if (rectTransform != null)
+              rectTransform.anchoredPosition = new Vector2(-map[0].Length * 50f + 50f, map.Length * 50f - 50f) +
+                                               new Vector2(x * 100f, -y * 100f);
+
+            playerVector = new Vector3(x, map.Length - y, 0);
+            break;
+          case 6:
+            var exit = Instantiate(floor_exit, new Vector3(x, map.Length - y, 0), Quaternion.identity);
+            exit.transform.SetParent(Parent.transform);
+            break;
         }
+      }
     }
+
     character.GetComponent<RectTransform>().position = playerVector;
   }
 
@@ -81,6 +79,7 @@ public class Map1 : MonoBehaviour
         map[i][j] = int.Parse(lines[i][j].ToString());
       }
     }
+
     return map;
   }
 }
