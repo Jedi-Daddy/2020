b727944b254858a926fb1aa0b0badeb5fabcf91e
Hrysh <hryshenko@gmail.com>
2020-10-24 23:44:33 +0300

    Add sound system. Add game over sound.

---
+++ Assets/Scripts/SoundSystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SoundSystem : MonoBehaviour
+{
+    [Serializable]
+    public class Sound
+    {
+        public string Name;
+        public AudioClip Clip;
+    }
+
+    public static SoundSystem Instance;
+
+    public AudioSource Source;
+    public Sound[] Sounds;
+
+    void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public void Play(string name)
+    {
+        var clip = Sounds.FirstOrDefault(x => x.Name.Equals(name));
+        if (clip == null)
+        {
+            Debug.LogError("Sound clip with name " + name + "not found");
+            return;
+        }
+
+        Source.PlayOneShot(clip.Clip);
+    }
+}
