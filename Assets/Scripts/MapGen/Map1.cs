b96927ab822d7269161a9656371f1ad1788d86ed
VShev <viky2503@gmail.com>
2020-10-24 09:04:50 +0300

    map 4 levels

---
+++ Assets/Scripts/MapGen/Map1.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class Map1 : MonoBehaviour
+{
+  public string level =
+@"1	1	0	1	6	0	0	0	0
+ 0	0	0	1	0	0	0	1	0
+ 0	1	0	0	0	1	0	1	0
+ 0	0	1	0	1	0	0	0	1
+ 1	0	1	0	0	0	1	0	1
+ 0	0	0	0	0	1	1	0	1
+ 0	1	0	0	1	1	0	0	0
+ 0	1	0	1	0	0	0	1	0
+ 0	0	0	0	0	1	1	0	0
+ 1	1	1	0	5	0	0	0	1";
+
+  public GameObject character;
+  public GameObject floor_valid;
+  public GameObject floor_obstacle;
+  public GameObject floor_exit;
+  public GameObject Parent;
+
+  public void Start()
+  {
+    var map = ReadFromFile(level);
+    for (int y = 0; y < map.Length; y++)
+    {
+      
+        for (int x = 0; x < map[y].Length; x++)
+        {
+          switch (map[y][x])
+          {
+            case 0:
+              var block = Instantiate(floor_valid, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              block.transform.SetParent(Parent.transform);
+              break;
+            case 1:
+              var obstacle = Instantiate(floor_obstacle, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              obstacle.transform.SetParent(Parent.transform);
+              break;
+            case 5:
+              var characterBlock = Instantiate(character, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              characterBlock.transform.SetParent(Parent.transform);
+              //var valid = Instantiate(floor_valid, new Vector3(map[y].Length-x, map.Length-y, 0), Quaternion.identity);
+              //valid.transform.SetParent(Parent.transform);
+              break;
+            case 6:
+              var exit = Instantiate(floor_exit, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              exit.transform.SetParent(Parent.transform);
+              break;
+          }
+        }
+      
+      }
+    }
+
+  public int[][] ReadFromFile(string level)
+  {
+    level = level.Replace("\t", "").Replace(" ", "");
+    string[] lines = Regex.Split(level, "\r\n");
+    int rows = lines.Length;
+
+    int[][] map = new int[rows][];
+    for (int i = 0; i < lines.Length; i++)
+    {
+      map[i] = new int[lines[i].Length];
+      for (int j = 0; j < lines[i].Length; j++)
+      {
+        var x = lines[i][j].ToString();
+        map[i][j] = int.Parse(lines[i][j].ToString());
+      }
+    }
+    return map;
+  }
+}
---
+++ Assets/Scripts/MapGen/Map3.cs
@@ -0,0 +1,114 @@
+﻿using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class Map3 : MonoBehaviour
+{
+  public string level =
+@"1	0	1	0	6	0	0	0	1
+1	0	0	0	0	0	1	0	1
+0	0	1	1	1	0	0	0	1
+1	0	0	0	0	1	0	1	1
+0	0	1	1	0	0	0	0	0
+0	1	1	1	1	0	1	1	0
+0	1	0	0	1	0	0	0	0
+0	0	0	0	0	1	0	1	0
+0	0	1	1	0	0	0	0	0
+1	0	0	0	5	0	1	0	1";
+
+  public GameObject character;
+  public GameObject floor_valid;
+  public GameObject floor_obstacle;
+  public GameObject floor_exit;
+
+  public GameObject floor_empty;
+  public GameObject character2;
+  public GameObject floor_valid2;
+  public GameObject floor_obstacle2;
+  public GameObject floor_exit2;
+  public GameObject Parent;
+
+  public void Start()
+  {
+    var map = ReadFromFile(level);
+    for (int y = 0; y < map.Length; y++)
+    {
+      if (y < 9)
+      {
+        for (int x = 0; x < map[y].Length; x++)
+        {
+          switch (map[y][x])
+          {
+            case 0:
+              var block = Instantiate(floor_valid, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              block.transform.SetParent(Parent.transform);
+              break;
+            case 1:
+              var obstacle = Instantiate(floor_obstacle, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              obstacle.transform.SetParent(Parent.transform);
+              break;
+            case 5:
+              var characterBlock = Instantiate(character, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              characterBlock.transform.SetParent(Parent.transform);
+              //var valid = Instantiate(floor_valid, new Vector3(map[y].Length-x, map.Length-y, 0), Quaternion.identity);
+              //valid.transform.SetParent(Parent.transform);
+              break;
+            case 6:
+              var exit = Instantiate(floor_exit, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              exit.transform.SetParent(Parent.transform);
+              break;
+            case 10:
+              var empty = Instantiate(floor_empty, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              empty.transform.SetParent(Parent.transform);
+              break;
+          }
+        }
+      }
+      else
+      {
+        for (int x = 0; x < map[y].Length; x++)
+        {
+          switch (map[y][x])
+          {
+            case 0:
+              var block = Instantiate(floor_valid2, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              block.transform.SetParent(Parent.transform);
+              break;
+            case 1:
+              var obstacle = Instantiate(floor_obstacle2, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              obstacle.transform.SetParent(Parent.transform);
+              break;
+            case 5:
+              var characterBlock = Instantiate(character2, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              characterBlock.transform.SetParent(Parent.transform);
+              //var valid = Instantiate(floor_valid, new Vector3(map[y].Length-x, map.Length-y, 0), Quaternion.identity);
+              //valid.transform.SetParent(Parent.transform);
+              break;
+            case 6:
+              var exit = Instantiate(floor_exit2, new Vector3(map[y].Length - x, map.Length - y, 0), Quaternion.identity);
+              exit.transform.SetParent(Parent.transform);
+              break;
+          }
+        }
+      }
+    }
+  }
+
+  public int[][] ReadFromFile(string level)
+  {
+    level = level.Replace("\t", "").Replace(" ", "");
+    string[] lines = Regex.Split(level, "\r\n");
+    int rows = lines.Length;
+
+    int[][] map = new int[rows][];
+    for (int i = 0; i < lines.Length; i++)
+    {
+      map[i] = new int[lines[i].Length];
+      for (int j = 0; j < lines[i].Length; j++)
+      {
+        var x = lines[i][j].ToString();
+        map[i][j] = int.Parse(lines[i][j].ToString());
+      }
+    }
+    return map;
+  }
+}
