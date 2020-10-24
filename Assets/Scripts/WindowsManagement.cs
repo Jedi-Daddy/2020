72949b5f039bc4e26a71113a497572dd914f761d
VShev <viky2503@gmail.com>
2020-10-24 09:55:50 +0300

    intro

--- Assets/Scripts/WindowsManagement.cs
+++ Assets/Scripts/WindowsManagement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class WindowsManagement : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class WindowsManagement : MonoBehaviour
   public GameObject Level2;
   public GameObject Level3;
   public GameObject Level4;
+  public GameObject Intro;
 
   void Start()
     {
@@ -15,8 +17,8 @@ public class WindowsManagement : MonoBehaviour
     Level2.active = false;
     Level3.active = false;
     Level4.active = false;
-
-
+    var introVideo = Intro.GetComponentInChildren<VideoPlayer>();
+    introVideo.Play();
   }
 
   public void StartLev1()
@@ -27,8 +29,32 @@ public class WindowsManagement : MonoBehaviour
     Level4.active = false;
   }
 
-    // Update is called once per frame
-    void Update()
+  public void StartLev2()
+  {
+    Level1.active = false;
+    Level2.active = true;
+    Level3.active = false;
+    Level4.active = false;
+  }
+
+  public void StartLev3()
+  {
+    Level1.active = false;
+    Level2.active = false;
+    Level3.active = true;
+    Level4.active = false;
+  }
+
+  public void StartLev4()
+  {
+    Level1.active = false;
+    Level2.active = false;
+    Level3.active = false;
+    Level4.active = true;
+  }
+
+  // Update is called once per frame
+  void Update()
     {
         
     }
