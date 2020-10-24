4299a7a578b49b7a7d6471412a55df37203fc66a
VShev <viky2503@gmail.com>
2020-10-24 17:42:14 +0300

    Update WindowsManagement.cs

--- Assets/Scripts/WindowsManagement.cs
+++ Assets/Scripts/WindowsManagement.cs
@@ -12,6 +12,8 @@ public class WindowsManagement : MonoBehaviour
   public GameObject Intro;
   public GameObject Loading;
 
+  private int currentLevel = 0;
+
   void Start()
     {
     Intro.active = false;
@@ -78,6 +80,15 @@ public class WindowsManagement : MonoBehaviour
     loadingVideo.loopPointReached += StartLev4;
   }
 
+  public void ShowEnd()
+  {
+    Intro.active = false;
+    Level1.active = false;
+    Level2.active = false;
+    Level3.active = false;
+    Level4.active = false;
+  }
+
   public void StartLev1(VideoPlayer vp)
   {
     vp.Stop();
@@ -87,6 +98,7 @@ public class WindowsManagement : MonoBehaviour
     Level3.active = false;
     Level4.active = false;
     Loading.active = false;
+    currentLevel = 1;
   }
 
   public void StartLev2(VideoPlayer vp)
@@ -97,6 +109,7 @@ public class WindowsManagement : MonoBehaviour
     Level2.active = true;
     Level3.active = false;
     Level4.active = false;
+    currentLevel = 2;
   }
 
   public void StartLev3(VideoPlayer vp)
@@ -107,6 +120,7 @@ public class WindowsManagement : MonoBehaviour
     Level2.active = false;
     Level3.active = true;
     Level4.active = false;
+    currentLevel = 3;
   }
 
   public void StartLev4(VideoPlayer vp)
@@ -117,7 +131,29 @@ public class WindowsManagement : MonoBehaviour
     Level2.active = false;
     Level3.active = false;
     Level4.active = true;
+    currentLevel = 4;
   }
+
+  public void ToNextLevel()
+  {
+    switch (currentLevel)
+    {
+      case 1:
+        ShowLoading2();
+        return;
+      case 2:
+        ShowLoading3();
+        return;
+      case 3:
+        ShowLoading4();
+        return;
+      case 4:
+        ShowEnd();
+        return;
+
+    }
+  }
+
 
   // Update is called once per frame
   void Update()
