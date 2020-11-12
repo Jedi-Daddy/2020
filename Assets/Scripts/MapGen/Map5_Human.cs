00862c46cb56582d263a3aa9830c7c99b8e800dd
Hrysh <hryshenko@gmail.com>
2020-11-12 16:22:50 +0200

    Update level balance.

--- Assets/Scripts/MapGen/Map5_Human.cs
+++ Assets/Scripts/MapGen/Map5_Human.cs
@@ -11,13 +11,13 @@ public class Map5_Human : MonoBehaviour, IMap
 @"2	0	0	0	0	0	0	0	6
 2	0	0	0	0	0	0	0	0
 0	0	2	2	0	1	1	0	0
-0	0	0	0	0	1	0	0	0
+1	0	0	0	0	1	0	0	0
 0	0	0	0	0	0	0	0	1
 0	0	0	2	0	2	2	0	1
-0	0	1	0	0	0	0	0	0
+0	1	1	0	0	0	0	0	0
 0	0	0	0	0	0	0	0	0
 0	0	0	1	0	0	0	0	0
-0	0	0	0	5	0	0	0	0";
+0	0	0	0	5	0	0	0	1";
 
   public GameObject character;
   public GameObject floor_valid;
